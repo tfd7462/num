0f96429f27364ab8732db73cfc3d8b0b6df963c6
unknown <fault1005@gmail.com>
2020-05-06 14:38:50 +0800

    sec files

--- r.py
+++ r.py
@@ -1,6 +1,8 @@
 import random 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1 
 	num = input("請輸入數字:")
 	num = int(num)
 	if num == r:
@@ -10,3 +12,4 @@ while True:
 		print("答錯了,比答案還大")
 	else:
 		print("答錯了,比答案還小")
+	print("這是你猜的第",count,"次")
